fa997636ee8fae954d738b76779d0af66506ced9
monmun018 <quymao08101999@gmail.com>
2024-06-18 15:16:11 +0900

    ＋JPAを追加 ＋Domainにテンプレート名を設定する。

--- src/main/java/com/example/domain/OptionGroup.java
+++ src/main/java/com/example/domain/OptionGroup.java
@@ -1,5 +1,7 @@
 package com.example.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,11 @@ import lombok.NoArgsConstructor;
  *
  * @author char5742
  */
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "option_groups")
 public class OptionGroup {
     private String id;
 }
--- src/main/java/com/example/domain/OrderItem.java
+++ src/main/java/com/example/domain/OrderItem.java
@@ -1,5 +1,7 @@
 package com.example.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,11 @@ import lombok.NoArgsConstructor;
  *
  * @author char5742
  */
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "order_items")
 public class OrderItem {
     private String id;
 }
