9395777a8e9dfa87fe546d458021aa9b59c3f79c
tugukurechan <tugukurechan@gmail.com>
2024-06-19 10:03:29 +0900

    docs: ddlの編集

--- document/myddl.sql
+++ document/myddl.sql
@@ -4,49 +4,52 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- ユーザー(users)
 drop table if exists users cascade;
 CREATE TABLE users (
- id serial PRIMARY KEY
- , name varchar(100) not null
- , email varchar(100) not null unique
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4()
+ , name text not null
+ , email text not null unique
  , password text not null
- , zipcode varchar(8) not null
- , prefecture varchar(10) not null
- , municipalities varchar(10) not null
- , address varchar(20) not null
- , telephone varchar(15) not null
+ , zipcode text not null
+ , prefecture text not null
+ , municipalities text not null
+ , address text not null
+ , telephone text not null
+ , created_at timestamp default CURRENT_TIMESTAMP
+ , updated_at timestamp default CURRENT_TIMESTAMP
 ) ;
 
 -- 注文(orders)
 drop table if exists orders cascade;
 CREATE TABLE orders (
- id serial PRIMARY KEY
- , user_id INTEGER
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4()
+ , user_id UUID
  , status INTEGER not null
  , total_price INTEGER not null
  , order_date text not null
- , destination_name varchar(20) default ''
- , destination_email varchar(255) not null
- , destination_zipcode varchar(20) not null
- , destination_prefecture varchar(20) not null
- , destination_municipalities varchar(20) not null
- , destination_address varchar(20) not null
- , destination_tel varchar(20)
- , destination_method varchar(15) not null
- , FOREIGN KEY (user_id) REFERENCES users(id)
+ , destination_name text default ''
+ , destination_email text not null
+ , destination_zipcode text not null
+ , destination_prefecture text not null
+ , destination_municipalities text not null
+ , destination_address text not null
+ , destination_tel text not null
+ , destination_method text not null
+ , created_at timestamp default CURRENT_TIMESTAMP
+ , updated_at timestamp default CURRENT_TIMESTAMP
 ) ;
 
 --------------------------------------------------------
 -- 犬種(breeds)
 drop table if exists breeds cascade;
 CREATE TABLE breeds (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) not null
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text not null
 );
 
 -- 色(colors)
 drop table if exists colors cascade;
 CREATE TABLE colors (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) not null
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text not null
 );
 
 -- 商品(items)
@@ -58,8 +61,10 @@ CREATE TABLE items (
   , image text not null
   , gender text not null
   , deleted boolean default false not null
-  , breed_id INTEGER
-  , color_id INTEGER
+  , breed_id UUID
+  , color_id UUID
+  , created_at timestamp default CURRENT_TIMESTAMP
+  , updated_at timestamp default CURRENT_TIMESTAMP
   , FOREIGN KEY (breed_id) REFERENCES breeds(id)
   , FOREIGN KEY (color_id) REFERENCES colors(id)
 ) ;
@@ -67,18 +72,18 @@ CREATE TABLE items (
 -- オプショングループ(option_groups)
 drop table if exists option_groups cascade;
 CREATE TABLE option_groups (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) not null,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text not null,
     input_type text not null
 );
 
 -- オプション(options)
-drop table if exists option cascade;
+drop table if exists options cascade;
 CREATE TABLE options (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(64) not null,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text not null,
     price Integer not null,
-    option_group_id INTEGER,
+    option_group_id UUID,
     FOREIGN KEY (option_group_id) REFERENCES option_groups(id)
 );
 
@@ -87,9 +92,9 @@ CREATE TABLE options (
 -- 注文された商品(order_items)
 drop table if exists order_items cascade;
 CREATE TABLE order_items (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     item_id UUID,
-    order_id INTEGER,
+    order_id UUID,
     FOREIGN KEY (item_id) REFERENCES items(id),
     FOREIGN KEY (order_id) REFERENCES orders(id)
 );
@@ -97,9 +102,9 @@ CREATE TABLE order_items (
 -- 注文された商品のオプション(order_item_options)
 drop table if exists order_item_options cascade;
 CREATE TABLE order_item_options(
-    id SERIAL PRIMARY KEY,
-    order_item_option INTEGER,
-    option_id INTEGER,
-    FOREIGN KEY (order_item_option) REFERENCES order_items(id),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    order_item_id UUID,
+    option_id UUID,
+    FOREIGN KEY (order_item_id) REFERENCES order_items(id),
     FOREIGN KEY (option_id) REFERENCES options(id)
 );
