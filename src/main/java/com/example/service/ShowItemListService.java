a5c3b85f7ec5b3dd595568f34f482be352292117
tugukurechan <tugukurechan@gmail.com>
2024-06-19 11:47:34 +0900

    feat: showItemListServiceクラスの作成とfindAll()の作成

--- src/main/java/com/example/service/ShowItemListService.java
+++ src/main/java/com/example/service/ShowItemListService.java
@@ -1,10 +1,29 @@
 
 package com.example.service;
 
-/**
- *
- * @author char5742
- */
-public class ShowItemListService {
+import com.example.domain.Item;
+import com.example.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+/**
+ * 商品を表示したり、検索をするサービスクラス.
+ *
+ * @author takeru.chugun
+ */
+@Service
+public class ShowItemListService {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    /**
+     * 全商品を取得する.
+     *
+     * @return 全商品
+     */
+    public List<Item> findAll(){
+        return itemRepository.findAll();
+    }
 }
