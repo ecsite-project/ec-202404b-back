b4bf30e4e319404b1dedc1f0d2c35b6a007b82fb
tugukurechan <tugukurechan@gmail.com>
2024-06-19 16:32:57 +0900

    OptionGroupRepositoryの作成

--- src/main/java/com/example/repository/OptionGroupRepository.java
+++ src/main/java/com/example/repository/OptionGroupRepository.java
@@ -1,2 +1,12 @@
-package com.example.repository;public class OptionGroupRepository {
+package com.example.repository;
+
+import com.example.domain.OptionGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface OptionGroupRepository extends JpaRepository<OptionGroup, UUID> {
 }
