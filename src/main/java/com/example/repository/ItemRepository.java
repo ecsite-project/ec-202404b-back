defac0276424b7dd92d521f1f679d6712dd31536
monmun018 <quymao08101999@gmail.com>
2024-06-21 12:09:25 +0900

    feat:ページング対応 レポジトリ

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import com.example.domain.Breed;
 import com.example.domain.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -21,4 +23,12 @@ public interface ItemRepository extends JpaRepository<Item,UUID>{
     List<Item> findByPriceBetweenAndColorIdIn(Double minPrice, Double maxPrice, List<UUID> colorIds);
     /** 上限価格、下限価格で検索 */
     List<Item> findByPriceBetween(Double minPrice, Double maxPrice);
+
+    /*---------ページング用---------*/
+    /** 上限価格、下限価格、種別、色で検索 */
+    Page<Item> findByPriceBetweenAndBreedIdAndColorIdIn(Double minPrice, Double maxPrice, UUID breed, List<UUID> colorIds, Pageable pageable);
+    /** 上限価格、下限価格、色で検索 */
+    Page<Item> findByPriceBetweenAndColorIdIn(Double minPrice, Double maxPrice, List<UUID> colorIds, Pageable pageable);
+    /** 上限価格、下限価格で検索 */
+    Page<Item> findByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
 }
