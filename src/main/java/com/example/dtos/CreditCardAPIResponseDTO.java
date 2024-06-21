22421e0f995d73b329cf6a9c47915f2c3db20456
monmun018 <quymao08101999@gmail.com>
2024-06-21 15:50:26 +0900

    feat:DTO

---
+++ src/main/java/com/example/dtos/CreditCardAPIResponseDTO.java
@@ -0,0 +1,22 @@
+package com.example.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author mao
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditCardAPIResponseDTO {
+    private String status;
+    private String message;
+    @JsonProperty("error_code")
+    private String errorCode;
+}
