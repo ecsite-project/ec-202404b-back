9fd1916a759a7ac368ec20e0076d720fb646dfbf
tugukurechan <tugukurechan@gmail.com>
2024-06-21 17:39:17 +0900

    テスト用に出力してた行の修正と、各ファイルの整形

--- src/main/java/com/example/service/CreditCardService.java
+++ src/main/java/com/example/service/CreditCardService.java
@@ -1,13 +1,17 @@
 package com.example.service;
 
-import com.example.dtos.PaymentInfoDTO;
-import com.example.dtos.CreditCardAPIResponseDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.example.dtos.CreditCardAPIResponseDTO;
+import com.example.dtos.PaymentInfoDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * @author mao
