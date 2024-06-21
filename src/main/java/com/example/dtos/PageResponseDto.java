2f8130e9cd8d605b8dc601cebbfd0a639a62e654
char5742 <fujino@stellarcode.jp>
2024-06-21 10:37:19 +0900

    add: ページング用ドメインの追加

---
+++ src/main/java/com/example/dtos/PageResponseDto.java
@@ -0,0 +1,36 @@
+package com.example.dtos;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * ページング情報のレスポンスドメイン.
+ * 
+ * @param <T> ページング対象のドメイン
+ * @author char5742
+ */
+@Data
+@AllArgsConstructor
+public final class PageResponseDto<T> {
+    /** メタデータ */
+    private Metadata metadata;
+    /** レコード */
+    private List<T> records;
+
+    @Data
+    @AllArgsConstructor
+    public final class Metadata {
+        /** 現在のページ番号 */
+        int currentPage;
+        /** ページあたりのレコード数 */
+        int perPage;
+        /** 最後のページ番号 */
+        int lastPage;
+        /** 総レコード数 */
+        int total;
+
+    }
+
+}
