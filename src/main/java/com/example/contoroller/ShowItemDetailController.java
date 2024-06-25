8072f2f63e0009b011a6cd998218ad1e1654027b
char5742 <fujino@stellarcode.jp>
2024-06-25 10:13:21 +0900

    fix: クケリパラメータで受け取れるように

--- src/main/java/com/example/contoroller/ShowItemDetailController.java
+++ src/main/java/com/example/contoroller/ShowItemDetailController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.service.ShowItemDetailService;
@@ -29,7 +30,7 @@ public class ShowItemDetailController {
      * @return 商品の詳細
      */
     @GetMapping("")
-    public ResponseEntity<?> showDetailPage(String id) {
+    public ResponseEntity<?> showDetailPage(@RequestParam("id") String id) {
         try {
             return ResponseEntity.ok(service.getDetail(UUID.fromString(id)));
         } catch (Exception e) {
