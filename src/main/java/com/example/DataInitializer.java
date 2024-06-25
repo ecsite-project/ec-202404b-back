65e7c90ebb818e12ee5465307be1288baf04f71f
tugukurechan <tugukurechan@gmail.com>
2024-06-25 14:31:58 +0900

    データの追加

--- src/main/java/com/example/DataInitializer.java
+++ src/main/java/com/example/DataInitializer.java
@@ -86,6 +86,19 @@ public class DataInitializer implements CommandLineRunner {
         breedRepository.save(Breed.builder().name("Beagle").build());
         breedRepository.save(Breed.builder().name("Chihuahua").build());
         breedRepository.save(Breed.builder().name("Dachshund").build());
+        breedRepository.save(Breed.builder().name("Munchkin").build());
+        breedRepository.save(Breed.builder().name("Scottish Fold").build());
+        breedRepository.save(Breed.builder().name("Sphynx").build());
+        breedRepository.save(Breed.builder().name("Russian Blue").build());
+        breedRepository.save(Breed.builder().name("Siamese").build());
+        breedRepository.save(Breed.builder().name("Maine Coon").build());
+        breedRepository.save(Breed.builder().name("Persian").build());
+        breedRepository.save(Breed.builder().name("Abyssinian").build());
+        breedRepository.save(Breed.builder().name("Norwegian Forest Cat").build());
+        breedRepository.save(Breed.builder().name("Bengal").build());
+        breedRepository.save(Breed.builder().name("etc").build());
+
+
     }
 
     @Autowired
@@ -103,7 +116,7 @@ public class DataInitializer implements CommandLineRunner {
         colorRepository.save(Color.builder().name("Red").build());
         colorRepository.save(Color.builder().name("Cream").build());
         colorRepository.save(Color.builder().name("Fawn").build());
-        colorRepository.save(Color.builder().name("Blue").build());
+        colorRepository.save(Color.builder().name("Pink").build());
     }
 
     @Autowired
@@ -114,84 +127,174 @@ public class DataInitializer implements CommandLineRunner {
             return;
         }
         itemRepository.save(Item.builder()
-                .description("Friendly Shiba Inu")
+                .description("柴犬は活発で忠実、賢い犬種です。勇敢で自立心が強く、しつけが比較的容易です。小型犬ながらも運動能力が高く、独特の鳴き声が特徴です。日本原産の犬種で、その美しい姿と忠実な性格から日本国内外で人気があります。")
                 .price(300000)
-                .image("shiba_inu.png")
+                .image("shiba_inu.jpg")
                 .gender("male")
                 .birthDay(LocalDate.of(2023, 1, 1))
                 .breed(breedRepository.findByName("Shiba Inu"))
-                .color(colorRepository.findByName("Black")).build());
+                .color(colorRepository.findByName("Brown")).build());
         itemRepository.save(Item.builder()
-                .description("Loyal Labrador")
+                .description("ラブラドール・レトリバーは快活で友好的、知識が豊富で活発な犬種です。知的で訓練しやすく、家庭犬や介助犬として広く用いられています。優れた嗅覚と水泳能力を持ち、穏やかで家族に対する愛情深い性格が特徴です。")
                 .price(250000)
-                .image("labrador.png")
+                .image("labrador.jpg")
                 .gender("female")
                 .birthDay(LocalDate.of(2023, 2, 1))
                 .breed(breedRepository.findByName("Labrador Retriever"))
-                .color(colorRepository.findByName("Black")).build());
+                .color(colorRepository.findByName("Golden")).build());
 
         itemRepository.save(Item.builder()
-                .description("Brave German Shepherd")
+                .description("ジャーマン・シェパードは、賢く忠実で勇敢な警察犬として知られる犬種です。知的で訓練しやすく、家庭での番犬や捜索犬としても適しています。")
                 .price(280000)
-                .image("german_shepherd.png")
+                .image("german_shepherd.jpg")
                 .gender("male")
                 .birthDay(LocalDate.of(2023, 3, 1))
                 .breed(breedRepository.findByName("German Shepherd"))
                 .color(colorRepository.findByName("Black")).build());
 
         itemRepository.save(Item.builder()
-                .description("Gentle Golden Retriever")
+                .description("ゴールデン・レトリバーは友好的で忠実、知識が豊富で社交的な犬種です。優れた家庭犬として知られ、子供や他のペットとの相性が良いです。穏やかで愛情深く、頭の良さと繊細な性格が魅力です。美しいゴールデンの被毛と笑顔が特徴的です。")
                 .price(270000)
-                .image("golden_retriever.png")
+                .image("golden_retriever.jpg")
                 .gender("female")
                 .birthDay(LocalDate.of(2023, 4, 1))
                 .breed(breedRepository.findByName("Golden Retriever"))
                 .color(colorRepository.findByName("Golden")).build());
 
         itemRepository.save(Item.builder()
-                .description("Playful Bulldog")
+                .description("ブルドックは頑丈で力強く、穏やかで愛情深い犬種です。マスキュリンでありながらも家族を大切にし、独特の容姿とゆったりとした歩調が特徴です。短く平たい顔、ひきしまった体つきが特徴で、その見た目とは裏腹に甘えん坊な一面も持っています。")
                 .price(260000)
-                .image("bulldog.png")
+                .image("bulldog.jpg")
                 .gender("male")
                 .birthDay(LocalDate.of(2023, 5, 1))
                 .breed(breedRepository.findByName("Bulldog"))
                 .color(colorRepository.findByName("White")).build());
 
         itemRepository.save(Item.builder()
-                .description("Smart Poodle")
+                .description("プードルは知的で活発、賢く訓練しやすい犬種です。美しいカールした被毛とスマートな容姿が特徴で、様々なサイズで飼育されています。絶え間ない動きと活気に満ち、高い知能と愛情深い性格で家族との絆を築きます。パフォーマンスやショーでの成功も多いです。")
                 .price(240000)
-                .image("poodle.png")
+                .image("poodle.jpg")
                 .gender("female")
                 .birthDay(LocalDate.of(2023, 6, 1))
                 .breed(breedRepository.findByName("Poodle"))
-                .color(colorRepository.findByName("Brown")).build());
+                .color(colorRepository.findByName("White")).build());
 
         itemRepository.save(Item.builder()
-                .description("Energetic Beagle")
+                .description("ビーグルは愛嬌があり快活で社交的、好奇心旺盛な犬種です。嗅覚が優れており、その能力を活かした捜索犬としても知られています。可愛らしい顔つきと耳、そして活発な性格が特徴で、子供や他の犬とも仲良く遊ぶことができます。スヌーピーのモデルになった犬種です！")
                 .price(230000)
-                .image("beagle.png")
+                .image("beagle.jpg")
                 .gender("male")
                 .birthDay(LocalDate.of(2023, 7, 1))
                 .breed(breedRepository.findByName("Beagle"))
-                .color(colorRepository.findByName("Black")).build());
+                .color(colorRepository.findByName("Golden")).build());
 
         itemRepository.save(Item.builder()
-                .description("Tiny Chihuahua")
+                .description("チワワは小型で活発、勇敢で警戒心が強い犬種です。家族に対しては非常に忠実で、独特の鳴き声が特徴です。小さい体格ながらも大胆で勇気があり、可愛らしい容姿と活発な性格が愛されます。")
                 .price(220000)
-                .image("chihuahua.png")
+                .image("chihuahua.jpg")
                 .gender("female")
                 .birthDay(LocalDate.of(2023, 8, 1))
                 .breed(breedRepository.findByName("Chihuahua"))
-                .color(colorRepository.findByName("Brown")).build());
+                .color(colorRepository.findByName("Black")).build());
 
         itemRepository.save(Item.builder()
-                .description("Long Dachshund")
+                .description("ダックスフンドは体格が長く、勇敢で自信に満ちた犬種です。狩猟犬としての能力があり、その優れた臭覚が特徴です。小型犬ながらもエネルギッシュで活発、愛嬌があり家族に対して愛情深い性格です。")
                 .price(210000)
-                .image("dachshund.png")
+                .image("dachshund.jpg")
                 .gender("male")
                 .birthDay(LocalDate.of(2023, 9, 1))
                 .breed(breedRepository.findByName("Dachshund"))
                 .color(colorRepository.findByName("Black")).build());
+
+        itemRepository.save(Item.builder()
+                .description("スコティッシュフォールドは特徴的な折れ耳が特徴で、温和で穏やかな性格です。人懐っこく、家族との時間を大切にする傾向があります。可愛らしい折れ耳と丸い顔つきが特徴であり、そのかわいらしい容姿と穏やかな性格が人気の理由です。")
+                .price(320000)
+                .image("scottishfold.jpg")
+                .gender("male")
+                .birthDay(LocalDate.of(2023, 11, 12))
+                .breed(breedRepository.findByName("Scottish Fold"))
+                .color(colorRepository.findByName("White")).build());
+
+        itemRepository.save(Item.builder()
+                .description("メインクイーンは大柄で力強く、友好的で社交的な性格を持つ猫種です。知恵があり、しばしば「犬のような」性格と形容されます。独特の外見と大きな体躯、フレンドリーで愛情深い性格が特徴であり、家族や他のペットとの親密な関係を築きます。")
+                .price(254000)
+                .image("mainecoon.jpg")
+                .gender("male")
+                .birthDay(LocalDate.of(2023, 10, 8))
+                .breed(breedRepository.findByName("Maine Coon"))
+                .color(colorRepository.findByName("Brown")).build());
+
+        itemRepository.save(Item.builder()
+                .description("ペルシャは長毛で美しい容姿を持つ、穏やかで温和な性格の猫種です。静かで愛情深く、飼い主に忠実です。豪華な被毛と大きな目、穏やかな性格が魅力であり、その美しい外見と落ち着いた性格が人気を集めます。")
+                .price(350000)
+                .image("persian.png")
+                .gender("female")
+                .birthDay(LocalDate.of(2024, 5, 4))
+                .breed(breedRepository.findByName("Persian"))
+                .color(colorRepository.findByName("Gray")).build());
+
+        itemRepository.save(Item.builder()
+                .description("アビシニアンは活発で好奇心旺盛、賢く社交的な性格を持つ猫種です。遊び好きで知的であり、活発なライフスタイルを好みます。細身でグレースフルな体つきと短い被毛、好奇心旺盛で遊ぶことが大好きな性格が特徴です。")
+                .price(322000)
+                .image("abyssinian.jpg")
+                .gender("female")
+                .birthDay(LocalDate.of(2024, 5, 12))
+                .breed(breedRepository.findByName("Abyssinian"))
+                .color(colorRepository.findByName("Brown")).build());
+
+        itemRepository.save(Item.builder()
+                .description("シャムは細身でエレガント、賢く活発な性格の猫種です。飼い主に対して忠実で、コミュニケーションを重視します。美しいブルーの目とショートヘアの被毛、活発で社交的な性格が特徴であり、愛らしい声でコミュニケーションをとることが知られています。")
+                .price(180000)
+                .image("siamese.jpg")
+                .gender("male")
+                .birthDay(LocalDate.of(2024, 4, 13))
+                .breed(breedRepository.findByName("Siamese"))
+                .color(colorRepository.findByName("White")).build());
+
+        itemRepository.save(Item.builder()
+                .description("ノルウェージャンフォレストキャットは 大柄で力強く、愛情深く忍耐強い性格の猫種です。冒険好きで、活動的なライフスタイルを好みます。高い適応能力と自然に溶け込む能力、美しい長毛と大きな体躯が特徴であり、寒い環境に適応する能力が高いです。")
+                .price(210000)
+                .image("norwegianforestcat.jpg")
+                .gender("female")
+                .birthDay(LocalDate.of(2024, 3, 13))
+                .breed(breedRepository.findByName("Norwegian Forest Cat"))
+                .color(colorRepository.findByName("Gray")).build());
+
+        itemRepository.save(Item.builder()
+                .description("ベンガルは野性的な外見と穏やかで社交的な性格を持つ猫種です。活発で好奇心旺盛、遊びと冒険を愛します。美しい豹柄模様の被毛とアクティブな性格が特徴であり、知的で愛情深い一面も持ち合わせています。")
+                .price(210000)
+                .image("bengal.jpg")
+                .gender("male")
+                .birthDay(LocalDate.of(2023, 6, 2))
+                .breed(breedRepository.findByName("Bengal"))
+                .color(colorRepository.findByName("Brown")).build());
+
+        itemRepository.save(Item.builder()
+                .description("穏やかで静か、知的で親密な性格を持つ猫種です。被毛は短く密でシルバーグレーの色合いが美しいです。美しい被毛とエレガントな容姿、静かで親しみやすい性格が特徴であり、飼い主との絆を大切にします。")
+                .price(210000)
+                .image("russianblue.jpg")
+                .gender("female")
+                .birthDay(LocalDate.of(2022, 2, 27))
+                .breed(breedRepository.findByName("Russian Blue"))
+                .color(colorRepository.findByName("Gray")).build());
+
+        itemRepository.save(Item.builder()
+                .description("穏やかで静か、知的で親密な性格を持つ猫種です。被毛は短く密でシルバーグレーの色合いが美しいです。美しい被毛とエレガントな容姿、静かで親しみやすい性格が特徴であり、飼い主との絆を大切にします。")
+                .price(210000)
+                .image("sphynx.jpg")
+                .gender("male")
+                .birthDay(LocalDate.of(2024, 5, 11))
+                .breed(breedRepository.findByName("Sphynx"))
+                .color(colorRepository.findByName("Pink")).build());
+
+        itemRepository.save(Item.builder()
+                .description("体は小柄で、丸い顔と大きな目が特徴です。短毛で柔らかい被毛を持ち、グレーと白の模様が綺麗に分かれています。画像のように立ち上がった姿勢が印象的です。メムキャットは非常に遊び好きで、特に動くおもちゃや光に反応することが多いです。家の中を走り回って遊ぶのが大好きですこの猫の一番の魅力は、その可愛らしい外見です。特に立ち上がった姿勢は、多くの人の心を引きつけるでしょう。。")
+                .price(800)
+                .image("nekomeme.jpg")
+                .gender("male")
+                .birthDay(LocalDate.of(2024, 4, 10))
+                .breed(breedRepository.findByName("etc"))
+                .color(colorRepository.findByName("Cream")).build());
     }
 
     @Autowired
@@ -404,7 +507,7 @@ public class DataInitializer implements CommandLineRunner {
                 .build());
 
         val item1 = itemRepository.save(Item.builder()
-                .description("Item1")
+                .description("Orderにあらかじめ追加するためのitemのサンプル")
                 .price(210000)
                 .image("dachshund.png")
                 .gender("male")
